ae12e6d419c4b40f00c9baddf8399e9516d3c97c
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-09 18:27:44 +0530

    create contact

---
+++ UC1/Person.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC1
+{
+    public class Person
+    {
+        public static void CreateContacts()
+        {
+            Contacts contacts = new Contacts();
+            Console.WriteLine("Enter First Name");
+            contacts.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name");
+            contacts.LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter EmailID ");
+            contacts.Email = Console.ReadLine();
+
+            Console.WriteLine("Enter Address");
+            contacts.Address = Console.ReadLine();
+
+            Console.WriteLine("Enter City");
+            contacts.City = Console.ReadLine();
+
+            Console.WriteLine("Enter State");
+            contacts.State = Console.ReadLine();
+
+            Console.WriteLine("Enter Zipcode ");
+            contacts.zipcode = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Enter Phone Number");
+            contacts.PhoneNumber =Convert.ToInt32 (Console.ReadLine());
+
+            Console.WriteLine("Created new contact \n");
+
+        }
+
+    }
+}
---
+++ UC1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UC1
+{
+    public class Program
+    {
+        public static List<Contacts> Person = new List<Contacts>();
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to AddressBook...!");
+            UC1.Person.CreateContacts();
+
+
+
+
+        }
+    }
+}
