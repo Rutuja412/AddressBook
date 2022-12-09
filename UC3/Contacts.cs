c7548617015f099782d7af6e1479094fa359c41d
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-09 20:53:23 +0530

    Edit existing contact

---
+++ UC3/Contacts.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC3
+{
+    public class Contacts
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string zipcode { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+}
---
+++ UC3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace UC3
+{
+    class Program
+    {
+        // public static List<Contacts> Person = new List<Contacts>();
+
+        public static void Main(string[] args)
+        {
+            //Console.WriteLine("welcome Address book");
+            UC3.Person.CreateContacts();
+
+            Person.editContact();
+        }
+    }
+}
