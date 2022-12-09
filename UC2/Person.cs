f449e4482ec6057edf7802c001eceb35042b1fb8
Rutuja412 <rutujakadam4545@gmail.com>
2022-12-09 20:17:56 +0530

    Add contacts in AddressBook

---
+++ UC2/Person.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UC2
+{
+    public class Person
+    {
+        public static List<Contacts> person = new List<Contacts>();
+        public static void CreateContacts()
+        {
+            Contacts contacts = new Contacts();
+            Console.WriteLine("Enter First Name");
+            contacts.FirstName = Console.ReadLine();
+
+            Console.WriteLine("Enter Last Name");
+            contacts.LastName = Console.ReadLine();
+
+            Console.WriteLine("Enter EmailID ");
+            contacts.Email = Console.ReadLine();
+
+            Console.WriteLine("Enter Address");
+            contacts.Address = Console.ReadLine();
+
+            Console.WriteLine("Enter City");
+            contacts.City = Console.ReadLine();
+
+            Console.WriteLine("Enter State");
+            contacts.State = Console.ReadLine();
+
+            Console.WriteLine("Enter Zipcode ");
+            contacts.zipcode = Console.ReadLine();
+
+            Console.WriteLine("Enter Phone Number");
+            contacts.PhoneNumber = Console.ReadLine();
+
+            Console.WriteLine("Created new contact \n");
+
+
+            Program.Person.Add(contacts);
+            Console.WriteLine("Contact added to your Address book");
+        }
+    }
+}
---
+++ UC2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace UC2
+{
+    public class Program
+    {
+        public static List<Contacts> Person = new List<Contacts>();
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("welcome Address book");
+            UC2.Person.CreateContacts();
+
+            
+        }
+    }
+}
